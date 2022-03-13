7f5612fba5d02ad91ffa2122df1c75aa0a78f582
SodaCris <18463922396@163.com>
2022-03-13 21:56:02 +0800

    add go

---
+++ robot.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/go-vgo/robotgo"
+	"github.com/vcaesar/imgo"
+)
+
+func main() {
+	x, y := robotgo.GetMousePos()
+	fmt.Println("pos: ", x, y)
+
+	color := robotgo.GetPixelColor(100, 200)
+	fmt.Println("color---- ", color)
+
+	sx, sy := robotgo.GetScreenSize()
+	fmt.Println("get screen size: ", sx, sy)
+
+	bit := robotgo.CaptureScreen(10, 10, 30, 30)
+	defer robotgo.FreeBitmap(bit)
+
+	img := robotgo.ToImage(bit)
+	imgo.Save("test.png", img)
+
+	img1 := robotgo.CaptureImg()
+	robotgo.Save(img1, "img.png")
+}
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,38 @@
+const builtin = @import("builtin");
 const std = @import("std");
+const io = std.io;
+const fmt = std.fmt;
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
+pub fn main() !void {
+    const stdout = io.getStdOut().writer();
+    const stdin = io.getStdIn();
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+    try stdout.print("Welcome to the Guess Number Game in Zig.\n", .{});
+
+    const answer = std.crypto.random.intRangeLessThan(u8, 0, 100) + 1;
+
+    while (true) {
+        try stdout.print("Guess a number between 1 and 100: ", .{});
+        var line_buf: [20]u8 = undefined;
+
+        const amt = try stdin.read(&line_buf);
+        if (amt == line_buf.len) {
+            try stdout.print("Input too long.\n", .{});
+            continue;
+        }
+        const line = std.mem.trimRight(u8, line_buf[0..amt], "\r\n");
+
+        const guess = fmt.parseUnsigned(u8, line, 10) catch {
+            try stdout.print("Invalid number.\n", .{});
+            continue;
+        };
+        if (guess > answer) {
+            try stdout.print("Guess lower.\n", .{});
+        } else if (guess < answer) {
+            try stdout.print("Guess higher.\n", .{});
+        } else {
+            try stdout.print("You win!\n", .{});
+            return;
+        }
+    }
 }
